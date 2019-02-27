d219ef2fefe2375a707cde6bf02c5a6232f33ff5
Breackey <brendaawino41@gmail.com>
2019-02-27 17:11:36 +0300

    added css and  javascript files

--- BBRS/pages/views.py
+++ BBRS/pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "home.html",{})
+def index(request):
+    return render(request, "index.html",{})
 
 def bookbus(request):
     return render(request, "bookbus.html",{})
